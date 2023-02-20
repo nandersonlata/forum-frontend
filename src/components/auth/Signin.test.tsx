a9ff362a090cf8457d6c997acdaa077e1b8f777d
Noah Anderson <nanderson@INT-ML-026.local>
2023-02-20 13:39:05 -0700

    Added another test and redirect functionality to Signin.tsx

--- src/components/auth/Signin.test.tsx
+++ src/components/auth/Signin.test.tsx
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import SignIn from './Signin';
 import { MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import axios, { AxiosStatic } from 'axios';
+
+interface AxiosMock extends AxiosStatic {
+  mockResolvedValue: Function;
+  mockRejectedValue: Function;
+}
+
+jest.mock('axios');
+const mockedAxios = axios as AxiosMock;
 
 describe('Signin component', () => {
   it('should not allow a signin click if email and password are not provided', () => {
@@ -31,5 +40,27 @@ describe('Signin component', () => {
 
     const signInButton = screen.getByText('Sign In');
     expect(signInButton).toBeEnabled();
+  });
+
+  it('inputs should have red borders when login fails', async () => {
+    mockedAxios.mockRejectedValue({ status: 403 });
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter initialEntries={['/signin']}>
+        <SignIn />
+      </MemoryRouter>,
+    );
+
+    const emailInput = screen.getByLabelText('Email Address', { exact: false });
+    await user.type(emailInput, 'test@email.com');
+
+    const passwordInput = screen.getByLabelText('Password', { exact: false });
+    await user.type(passwordInput, 'testPassword');
+
+    const signInButton = screen.getByText('Sign In');
+    await user.click(signInButton);
+
+    expect(emailInput.closest('div')).toHaveClass('Mui-error');
+    expect(passwordInput.closest('div')).toHaveClass('Mui-error');
   });
 });
--- src/components/auth/Signin.tsx
+++ src/components/auth/Signin.tsx
@@ -8,104 +8,111 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {useState} from "react";
-import {Grid, Link} from "@mui/material";
-import {addTokensFromResponseToLocalStorage} from "./util";
+import { useState } from 'react';
+import { Grid, Link } from '@mui/material';
+import { addTokensFromResponseToLocalStorage } from './util';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const theme = createTheme();
 
-const axios = require('axios');
-
 export default function SignIn() {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [invalidLogin, setInvalidLogin] = useState<boolean>(false);
+  const [email, setEmail] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const [invalidLogin, setInvalidLogin] = useState<boolean>(false);
 
-    function handleEmailChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        setEmail(event.target.value);
+  const navigate = useNavigate();
+
+  function handleEmailChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/auth/signin', {
+        email,
+        password,
+      });
+
+      addTokensFromResponseToLocalStorage(response);
+
+      navigate('/home');
+    } catch (error) {
+      setInvalidLogin(true);
     }
+  };
 
-    function handlePasswordChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        setPassword(event.target.value);
-    }
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:3000/auth/signin', {
-                email,
-                password
-            });
-
-            addTokensFromResponseToLocalStorage(response);
-
-            setInvalidLogin(false);
-        } catch (error) {
-            setInvalidLogin(true);
-        }
-    };
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Login
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            onChange={handleEmailChange}
-                            error={invalidLogin}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            onChange={handlePasswordChange}
-                            error={invalidLogin}
-                        />
-                        <Button
-                            id="sign-in-button"
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            disabled={!email && !password}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid item>
-                            <Link href="/signup" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleEmailChange}
+              error={invalidLogin}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handlePasswordChange}
+              error={invalidLogin}
+            />
+            <Button
+              id="sign-in-button"
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={!password || !email}
+            >
+              Sign In
+            </Button>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
