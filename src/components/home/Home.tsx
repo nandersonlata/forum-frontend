219bbde35f1927cdddbc6c0afcfda3fbdb92b67d
Noah Anderson <nanderson@INT-ML-026.local>
2023-02-23 14:11:57 -0700

    Added button to navigate to profile

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,12 @@
-import { Box, createTheme, ThemeProvider } from '@mui/material';
+import { Box, createTheme, IconButton, ThemeProvider } from '@mui/material';
 import Container from '@mui/material/Container';
 import React from 'react';
 import Logout from '../auth/Logout';
+import Person2Icon from '@mui/icons-material/Person2';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
   const theme = createTheme();
 
   return (
@@ -16,6 +19,9 @@ export default function Home() {
             justifyContent: 'flex-end',
           }}
         >
+          <IconButton>
+            <Person2Icon onClick={() => navigate('/profile')} />
+          </IconButton>
           <Logout />
         </Box>
       </Container>
