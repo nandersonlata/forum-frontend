2911deea45713ac8f0e1eb8fb903d4698dbdc200
Noah Anderson <noah.anderson@integral.io>
2023-04-19 10:52:34 -0600

    Removed unneccesary ternary in profile.tsx

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -42,26 +42,27 @@ export default function Profile() {
           }}
         >
           {loggedInUser.displayName.length > 0 &&
-          url.pathname.endsWith(loggedInUser.displayName) ? (
-            <Box aria-label={'profile-settings'}>
-              <Typography component="h1" variant="h5">
-                Update Profile Settings
-              </Typography>
-              <Link to="/profile/deactivate" style={{ textDecoration: 'none' }}>
-                <Button
-                  id="sign-in-button"
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
+            url.pathname.endsWith(loggedInUser.displayName) && (
+              <Box aria-label={'profile-settings'}>
+                <Typography component="h1" variant="h5">
+                  Update Profile Settings
+                </Typography>
+                <Link
+                  to="/profile/deactivate"
+                  style={{ textDecoration: 'none' }}
                 >
-                  Deactivate Account
-                </Button>
-              </Link>
-            </Box>
-          ) : (
-            <div></div>
-          )}
+                  <Button
+                    id="sign-in-button"
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Deactivate Account
+                  </Button>
+                </Link>
+              </Box>
+            )}
         </Box>
       </Container>
     </ThemeProvider>
